90d593d86dce99d54c7de5b30d74ca4468a10b5c
masaaki1915 <masaaki1915@gmail.com>
2022-07-28 02:09:33 +0900

    modify styles, add icon and links

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
 import type { Component } from 'solid-js';
 
-import logo from './logo.svg';
+import icon from './assets/icon.png';
 import styles from './App.module.css';
 
 const App: Component = () => {
   return (
     <div class={styles.App}>
       <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
+        <img src={icon} class={styles.icon} alt="icon" />
+        <p>ma96o</p>
+		<div class={styles.links}>
+		<a
           class={styles.link}
-          href="https://github.com/solidjs/solid"
+          href="https://github.com/ma96o"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Solid
+          Github
         </a>
+		/
+        <a
+          class={styles.link}
+          href="https://ma96o.github.io/gialog/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          dialy
+        </a>
+		</div>
       </header>
     </div>
   );
